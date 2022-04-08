f11d6b379edcf4665b4c1df820f73b1391812ab5
Lucas Freitas <lucasfrtsfilho@gmail.com>
2022-04-08 16:47:18 -0300

    trabalho de php aula 3
    trabalho desenvolvido em aula

---
+++ modelo.php
@@ -0,0 +1,24 @@
+<?php
+
+// Inclui cabeçalho php
+require('_header.php')
+?>
+            <section>
+
+                <h2>Título da pagina</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nulla reiciendis, repellat iusto id quaerat veniam accusantium ratione quas aperiam fugit nihil. Cum doloribus debitis quasi repellat pariatur odit deleniti.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, doloremque. Necessitatibus eveniet obcaecati minus, nobis aliquid ab tempora eligendi, esse impedit amet aspernatur? Sunt atque itaque, officia voluptates commodi quaerat.</p>
+           
+            </section>
+
+            <aside>
+                <h3>lateral</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur
+                    iusto.</p>
+
+            </aside>
+
+        </main>
+        <?php require('_footer.php')
+        
+        ?>
