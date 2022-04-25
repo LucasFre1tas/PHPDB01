1f3871625739c89c7f206f4f0cfc6ab276d3aade
Lucas Freitas <lucasfrtsfilho@gmail.com>
2022-04-25 13:36:09 -0300

    aula site completo php 8

--- _footer.php
+++ _footer.php
@@ -1,15 +1,17 @@
 </main>
 
 <footer>
-    <a href="/" title="Página inicial"><i class="fa-solid fa-house-chimney"></i></a>
 
-    <div>&copy; Copyright 2022 Vitugo.</div>
+    <a href="/" title="Página inicial"><i class="fa-solid fa-house-chimney fa-fw"></i></a>
 
-    <a href="#top"title="Topo da página"><i class="fa-solid fa-circle-up"></i></a>
+    <div title="Todos os direitos reservados para Vitugo.">&copy; Copyright 2022 Vitugo.</div>
+
+    <a href="#top" title="Topo da página"><i class="fa-solid fa-circle-up fa-fw"></i></a>
+
 </footer>
 
 </div>
 
-
 </body>
+
 </html>
